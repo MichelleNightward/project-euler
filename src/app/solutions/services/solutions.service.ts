120b95c44d50cafda89a7c2f48f7ba01befc80bb
Michelle Rooy <michelle@ovuline.com>
2017-11-08 10:12:17 -0800

    adding documentation

--- src/app/solutions/services/solutions.service.ts
+++ src/app/solutions/services/solutions.service.ts
@@ -8,6 +8,11 @@ export class SolutionsService {
 
   // TODO: possibly find a better way to break up this soon to be large file
 
+  /**
+   * Finds even Fibonacci numbers given a limit to calculate to.
+   *
+   * @param limit
+   */
   findEvenFibonacciNumbers(limit:number): number{
     let sum: number = 0;
     let next: number = 0;
@@ -20,20 +25,39 @@ export class SolutionsService {
     return sum;
   }
 
-  fibonacciRecursive(limit:number){
-    if(limit <= 2){
-      return limit;
+  /**
+   * Finds next fibonacci number in a sequence given the current number.
+   *
+   * @param currentFib
+   */
+  fibonacciRecursive(currentFib:number){
+    if(currentFib <= 2){
+      return currentFib;
     }
-    return this.fibonacciRecursive(limit-2) + this.fibonacciRecursive(limit-1);
+    return this.fibonacciRecursive(currentFib-2) + this.fibonacciRecursive(currentFib-1);
   }
 
+  /**
+   * Checks whether number is even
+   *
+   * @param intToCheck
+   */
   isEven(intToCheck:number):boolean{
     return intToCheck%2 == 0;
   }
 
-  findLargestPrimeFactor(limit:number): number{
-    let largest: number = limit;
-    let div:number = limit;
+  /**
+   * Finds the largest prime factor to a given number.
+   *
+   * There are several ways to do this, and this originally used isPrime() to solve
+   * but it's unsustainable on a vast level. Feeding the function 4000000 needs to not
+   * tax the system so this has been refactored to accommodate large numbers.
+   *
+   * @param intToCheck
+   */
+  findLargestPrimeFactor(intToCheck:number): number{
+    let largest: number = intToCheck;
+    let div:number = intToCheck;
     let odd:number = 3;
     while (div % 2 == 0) {
       largest = 2;
@@ -54,7 +78,11 @@ export class SolutionsService {
     return largest;
   }
 
-
+  /**
+   * Checks if provided number is a prime number. This method is currently unused.
+   *
+   * @param intToCheck
+   */
   isPrime(intToCheck:number):boolean{
     if (intToCheck == 1){
       return true;
@@ -70,7 +98,12 @@ export class SolutionsService {
     return true;
   }
 
-  findMultiples(limit:number): number{
+  /**
+   * Finds the sum of all multiples of 3 and 5 below the provided number.
+   *
+   * @param limit
+   */
+  findSumOf3And5Multiples(limit:number): number{
     let sum: number = 0;
     for(let i = 0; i < limit; ++i) {
       if (i%3 == 0 || i%5 == 0){
@@ -80,9 +113,15 @@ export class SolutionsService {
     return sum;
   }
 
-  fizzBuzz(int:number):string{
+  /**
+   * A sime FizzBuzz game, creates a string that represents all numbers below the provided number replacing multiples
+   * of 3 with the word Fizz, multiples of 5 with the word Buzz, and multiples of both 3 and 5 with FizzBuzz.
+   *
+   * @param limit
+   */
+  fizzBuzz(limit:number):string{
     let fizzBuzzResult:string ="";
-    for (let i=1; i <= int; i++){
+    for (let i=1; i <= limit; i++){
       if (i%3 == 0 && i%5 == 0){
         fizzBuzzResult += "FizzBuzz ";
       } else if (i%3 == 0){
@@ -96,7 +135,14 @@ export class SolutionsService {
     return fizzBuzzResult;
   }
 
-  findBTLowestCommonAncestor(root: BinaryTree, val1: number, val2: number){
+  /**
+   * Finds the lowest common ancestor shared between to numbers given a binary tree.
+   *
+   * @param root
+   * @param val1
+   * @param val2
+   */
+  findBTLowestCommonAncestor(root: BinaryTree, val1: number, val2: number): BinaryTree{
     if (!root) {
       return null;
     }
